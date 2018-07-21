8c47bb5add17c4fb45500e377b14381a984abf23
benmen87 <sretrh@gmail.com>
2018-07-21 16:48:26 +0300

    fix empty model path string

--- train_denoise.py
+++ train_denoise.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import os 
+import pprint
 import sys
 import numpy as np
 import torch
@@ -19,6 +20,10 @@ import test_denoise
 USE_CUDA = torch.cuda.is_available()
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
+def _pprint(stuff):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(stuff)
+
 def get_train_valid_loaders(dataset_path, batch_size, noise):
    
     def pre_process_fn(_x): return normilize(nhwc_to_nchw(_x), 255) 
@@ -37,25 +42,28 @@ def get_train_valid_loaders(dataset_path, batch_size, noise):
     valid_loader = DataLoader(valid_loader, batch_size=1, shuffle=True)
     return train_loader, valid_loader
 
-def step(model, img, img_n, optimizer=None, criterion=None):
-    if optimizer is not None: 
-       optimizer.zero_grad()
-    output = model(img_n)
-    if criterion is not None:
-        loss = criterion(output[..., 3:-3, 3:-3], img[..., 3:-3, 3:-3])
-        if optimizer:
-            loss.backward()
-            optimizer.step()
-        return loss, output.cpu()
-    return float(output)
+def step(model, img, img_n, optimizer, criterion):
+    """ Train step !!"""
+    optimizer.zero_grad()
 
-def maybe_save_model(model, opt, schd, epoch, save_path, curr_val, other_values):
-    path = ''
+    output = model(img_n)
+    sc_n = model.forward_enc(img_n)
+    sc_tar = model.forward_enc(img)
+
+    loss = criterion(output[..., 3:-3, 3:-3], img[..., 3:-3, 3:-3], sc_n, sc_tar.data)
+
+    loss.backward()
+    optimizer.step()
+    return float(loss), output.cpu()
+
+def maybe_save_model(model, opt, schd, epoch, save_path, curr_val,
+        other_values, model_path=None):
+    path = model_path if model_path is not None else ''
     def no_other_values(other_values):
         return len(other_values) == 0
-    if no_other_values(other_values) or curr_val > min(other_values):
-        print('saving model...')
+    if no_other_values(other_values) or curr_val > max(other_values):
         path = save_train(save_path, model, opt, schd, epoch)
+        print('saving model at path %s'%path)
         clean(save_path, save_count=10)
     return path
 
@@ -63,11 +71,12 @@ def run_valid(model, data_loader, criterion, logdir):
     loss = 0
     psnr = 0
     for img, img_n in data_loader:
-        _loss, _out = step(model, img, img_n, criterion=criterion)
-        loss += float(_loss)
+        _out = model(img_n)
+        
+        loss += float(criterion(img.data, _out.data))
         psnr += common.psnr(img.data.cpu().numpy(), _out.data.cpu().numpy())
 
-    _, output = step(model, img, img_n, criterion=criterion)
+    output = model(img_n)
     np.savez(os.path.join(logdir, 'images'), IN=img.data.cpu().numpy(),
         OUT=output.data.cpu().numpy(), NOISE=img_n.data.cpu().numpy())
     return loss / len(data_loader), psnr / len(data_loader)
@@ -76,20 +85,27 @@ def train(model, args):
     
     optimizer = optim.Adam(model.parameters(), lr=args['learning_rate'])
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', verbose=True)
-    recon_loss = reconsturction_loss(factor=0.2, use_cuda=True)
     train_loader, valid_loader = get_train_valid_loaders(args['dataset_path'], args['batch_size'], args['noise'])
-    print(args)
+
+    #TODO(hillel): this is ugly fix it!! simple l2 dist for valid more complex for train
+    valid_loss =  reconsturction_loss(ssim_factor=args['ssim_factor'], use_cuda=True)
+    criterion = common.get_criterion() 
+
+    print('train args:')
+    _pprint(args)
+
     if args['load_path'] != '':
         ld_p = args['load_path']
         print('loading from %s'%ld_p)
         load_train(ld_p, model, optimizer, scheduler)        
         print('Done!')
-        
+
+    model_path = None
     _train_loss = []
     _valid_loss = []
     _valid_psnr = []
     running_loss = 0
-    valid_every = 1#int(0.1 * len(train_loader))
+    valid_every = int(0.05 * len(train_loader))
 
     itr = 0
     for e in range(args['epoch']):
@@ -97,25 +113,29 @@ def train(model, args):
         for img, img_n in train_loader:
             itr += 1
 
-            _loss, _ = step(model, img, img_n, optimizer, recon_loss)
+            _loss, _ = step(model, img, img_n, optimizer, criterion=criterion)
             running_loss += float(_loss)
 
-            if itr % valid_every == 0 or True:
+            if itr % valid_every == 0:
                 _train_loss.append(running_loss / valid_every)
                 _v_loss, _v_psnr = run_valid(model, valid_loader,
-                        recon_loss,args['save_dir'])
+                        valid_loss, args['save_dir'])
                 scheduler.step(_v_loss)
                 model_path = maybe_save_model(model, optimizer,
                         scheduler, e, args['save_dir'],
-                        _v_psnr, _valid_psnr)
+                        _v_psnr, _valid_psnr, model_path)
                 _valid_loss.append(_v_loss)
                 _valid_psnr.append(_v_psnr)
                 print("epoch {} train loss: {} valid loss: {}, valid psnr: {}".format(e,
                     running_loss / valid_every, _v_loss, _v_psnr))
                 running_loss = 0
-        return model_path, _valid_loss[-1], _valid_psnr[-1]
+    return model_path, _valid_loss[-1], _valid_psnr[-1]
 
 def build_model(args):
+
+    print("model args")
+    _pprint(args)
+
     model = LISTAConvDictADMM(
         num_input_channels=args['num_input_channels'],
         num_output_channels=args['num_output_channels'],
@@ -142,16 +162,18 @@ def main(args_file):
     args['train_args']['final_psnr'] = valid_psnr
     arguments.logdictargs(os.path.join(log_dir, 'params.json'), args)
 
-    psnrs, res = test_denoise.test(args['model_args'],
-         model_path,
-         args['train_args']['noise'], 
-         args['test_args']['testset_path']
-         )
-    args['test_args']['final_psnrs'] = psnrs
-    arguments.logdictargs(os.path.join(log_dir, 'params.json'), args)
-    for idx, ims in enumerate(res):
-       test_denoise.plot_res(ims[0], ims[1], ims[2], idx, args['train_args']['log_dir']
-)
+    if args['test_args']['testset_path']:
+        psnrs, res = test_denoise.test(args['model_args'],
+             model_path,
+             args['train_args']['noise'], 
+             args['test_args']['testset_path']
+             )
+        args['test_args']['final_psnrs'] = psnrs
+        arguments.logdictargs(os.path.join(log_dir, 'params.json'), args)
+        for idx, ims in enumerate(res):
+            test_denoise.plot_res(ims[0], ims[1], ims[2], idx, args['train_args']['log_dir'])
+    else:
+        print('no test path provided skipping test run')
 
 if __name__ == '__main__':
     import argparse
